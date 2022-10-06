9ab7387e7c84cb75f4ba421dfc0fd038bc76d54a
WiredG <thomas.nykjaer@live.dk>
2022-10-06 11:50:15 +0200

    .

--- client/skillTree/skillTree.js
+++ client/skillTree/skillTree.js
@@ -1,3 +1,12 @@
+let offsetX = 150;
+let offsetY = 50;
+let boxWidth = 50;
+let boxHeigt = 20;
+let lastPosX = document.getElementById("skillTreeDiv").clientWidth / 2;
+let lastPosy = document.getElementById("skillTreeDiv").clientHeight - 500;
+
+let longestBranchFirst;
+
 fetch("../../input.json")
     .then(response => response.json())
     .catch(err => { alert("No calls"); console.log(err) })
@@ -13,105 +22,77 @@ fetch("../../input.json")
         }
         document.getElementById("title").innerText = nameText;
 
+        longestBranchFirst = jsonData.abilityMatrix.sort((a, b) => {
+            let aLen = CountBranchSize(0, a, jsonData.abilityMatrix);
+            let bLen = CountBranchSize(0, b, jsonData.abilityMatrix);
+            //console.log(a, aLen, b, bLen);
+            return bLen - aLen;
+        })
+        console.log(longestBranchFirst);
+        left = 0;
+        right = 0;
 
-        let starters = determineStarters(jsonData.abilityMatrix);
-        let explored = [];
-
-        for (let i = 0; i < starters.length; i++) {
-            //console.log("Doing a starter");
-            //mainUl.appendChild(exploreMatrix(jsonData, starters[i], true));
+        for (let i = 0; i < longestBranchFirst.length; i++) {
+            if (longestBranchFirst[i].added != true) {
+                longestBranchFirst[i].added = true;
+                console.log(longestBranchFirst);
+                let level = 0;
+                addElement(jsonData.abilityMatrix, longestBranchFirst[i], i, level);
+            }
         }
 
     });
-let pos = 0;
-document.getElementById("move").addEventListener("click", event => {
-    pos += 10;
-    console.log(pos)
-    elmnt.offsetHeight = (pos) + "px";
-    elmnt.offsetWidth = (pos) + "px";
-});
 
-let elmnt = document.getElementById("test");
-function move() {
-    pos += 10;
-    console.log("pos")
-    console.log(elmnt);
-    console.log(pos + "px");
-    elmnt.style.top = pos + "px";
-    elmnt.style.left = pos + "px";
-}
-
-/*
-//Explores the matrix for requirements, note: Does not detect loops
-function exploreMatrix(jsonData, elementNr, state) {
-    state < 0 ? state = 0 : state
-    if (jsonData.character.abilities.contains(jsonData.abilities[elementNr].name)) {
-        //console.log(jsonData.abilities[elementNr].name + " bought")
-        state = 2;
-    }
-
-    //console.log("Entering ExploreMatrix");
-    let li = createTreeNode(elementNr, jsonData.abilities, state);
-
-    //console.log(jsonData.abilities[elementNr].name);
-    let found = false;
-    let ulTwo = document.createElement("ul");
-    for (let i = 0; i < jsonData.abilityMatrix.length; i++) {
-
-        //console.log(elementNr + " " + i + " : " + jsonData.abilityMatrix[elementNr][i]);
-
-        if (jsonData.abilityMatrix[i][elementNr] == 1) {
-            //console.log("Found  AT")
-            ulTwo.appendChild(exploreMatrix(jsonData, i, state - 1));
-            li.appendChild(ulTwo);
-            found = true;
+function addElement(abilityMatrix, element, elementNr) {
+    console.log("Start ", element[0])
+    createElement(element[0]);
+    for (let i = 1; i < element.length; i++) {
+        if (element[i]) {
+            console.log(abilityMatrix[i - 1])
+            //createElement(abilityMatrix[i - 1][0]);
+            addElement(abilityMatrix,)
         }
     }
-    if (found) {
-        return li;
-    } else {
-        //console.log("Not found");
-        return createTreeNode(elementNr, jsonData.abilities, state);
-    }
 }
 
-
-
-/*
-function createTreeNode(i, abilities, state) {
-    let li = document.createElement("li");
+function createElement(name) {
     let span = document.createElement("span");
-    span.setAttribute("class", "unselectable")
-    span.innerText = abilities[i].name;
-    //console.log(abilities[i].name + " state: " + state);
-    switch (state) {
-        case 0:
-            //console.log("Red")
-            span.setAttribute("style", "background-color: #EE9090;");
-            break;
-        case 1:
-            //console.log("Yellow")
-            span.setAttribute("style", "background-color: #EEEE90;");
-            break;
-        case 2:
-            //console.log("Green")
-            span.setAttribute("style", "background-color: #90EE90;");
-            break;
+    span.style.left = lastPosX + "px";
+    span.style.top = lastPosy + "px";
+    lastPosy -= offsetY - boxHeigt;
+    span.innerText = name;
+    findAndAdd(name);
+    document.getElementById("skillTreeDiv").appendChild(span);
+}
+
+function findAndAdd(name) {
+    for (let i = 0; i < longestBranchFirst.length; i++) {
+        if (longestBranchFirst[i] == name) {
+            longestBranchFirst[i].added = true;
+        }
     }
-    //console.log("FOUND")
+}
 
-    span.addEventListener("mouseover", (event) => {
-        document.getElementById("hoverName").innerText = abilities[i].name;
-        document.getElementById("hoverText").innerText = abilities[i].description;
-    });
-
-    li.appendChild(span);
-    return li;
-}*/
-
-Array.prototype.contains = function (target) {
+function CountBranchSize(count, element, abilityMatrix) {
+    let childLengths = [];
+    for (let i = 1; i < element.length; i++) {
+        if (element[i] == 1) {
+            //console.log(element, i - 1);
+            //console.log(abilityMatrix[i - 1])
+            childLengths.push(CountBranchSize(count, abilityMatrix[i - 1], abilityMatrix));
+        }
+    }
+    let longestChild = 0;
+    for (let i = 0; i < childLengths.length; i++) {
+        if (longestChild < childLengths[i]) {
+            longestChild = childLengths[i];
+        }
+    }
+    return count + longestChild + 1;
+}
+Array.prototype.contains = function (startNr, target) {
     //console.log(this + " : " + target);
-    for (let i = 0; i < this.length; i++) {
+    for (let i = startNr; i < this.length; i++) {
         if (this[i] == target) {
             return true;
         }
@@ -119,24 +100,27 @@ Array.prototype.contains = function (target) {
     return false;
 }
 
-//Returns a list of numbers corresponding to the starter abilities(Not required by another ability)
-function determineStarters(matrix) {
-    let starters = []
-    for (let i = 0; i < matrix[0].length; i++) {
-        let notFound = true;
-        for (let p = 0; p < matrix.length; p++) {
-            //console.log(i + "length: " + matrix[0].length + "    " + p + "length:" + matrix.length);
-            if (matrix[i][p] == 1) {
-                notFound = false;
-            }
-        }
-        if (notFound) {
-            starters.push(i);
-        }
-    }
-    //console.log(starters);
-    return starters;
+/*
+let pos = 0;
+document.getElementById("move").addEventListener("click", event => {
+pos += 10;
+console.log(pos)
+elmnt.offsetHeight = (pos) + "px";
+elmnt.offsetWidth = (pos) + "px";
+});
+
+let elmnt = document.getElementById("test");
+function move() {
+pos += 10;
+console.log("pos")
+console.log(elmnt);
+console.log(pos + "px");
+elmnt.style.top = pos + "px";
+elmnt.style.left = pos + "px";
 }
+*/
+
+
 /*
 var elmnt = document.getElementById("skillTreeDiv");
 let enableDrag = false;
