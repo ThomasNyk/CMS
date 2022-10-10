4e4bed1835af156f33d50cbe3d82ad219024684e
WiredG <thomas.nykjaer@live.dk>
2022-10-10 11:46:52 +0200

    Added uuid pkg

--- client/cms/cms.mjs
+++ client/cms/cms.mjs
@@ -1,18 +1,24 @@
-fetch("./input.json")
-    .then(response => response.json())
-    .catch(err => { alert("No calls"); console.log(err) })
-    .then(jsonData => {
-        populateFields(jsonData);
-    });
+if (document.cookie != "pasd") {
+    console.log("asdasd");
+    window.location.replace("../login/login.html");
+} else {
+    fetch("../../../input.json")
+        .then(response => response.json())
+        .catch(err => { alert("No calls"); console.log(err) })
+        .then(jsonData => {
+            populateFields(jsonData);
+        });
+}
+
 
 function populateFields(obj) {
-    document.getElementById("name").value = obj.character.name;
-    //console.log(obj.character.image)
+    //document.getElementById("name").value = obj.character.name;
+    console.log(obj.character.image)
     if (obj.character.image != undefined && obj.character.image != "") {
         document.getElementById("img").src = obj.character.image;
     }
 
-    let religionSelector = document.getElementById("religion");
+    /*let religionSelector = document.getElementById("religion");
     for (let i = 0; i < obj.religions.length; i++) {
         let option = document.createElement("option");
         option.innerText = obj.religions[i].name;
@@ -22,10 +28,10 @@ function populateFields(obj) {
     if (obj.character.religion != "") {
         document.getElementById(obj.character.religion).setAttribute("selected", true);
         removeSelect();
-    }
+    }*/
 }
 
-document.getElementById("religion").addEventListener("click", removeSelect);
+//document.getElementById("religion").addEventListener("click", removeSelect);
 
 function removeSelect() {
     document.getElementById("removableReligion").remove();
--- client/skillTree/skillTree.js
+++ client/skillTree/skillTree.js
@@ -1,11 +1,14 @@
-let offsetX = 150;
-let offsetY = 50;
-let boxWidth = 50;
-let boxHeigt = 20;
-let lastPosX = document.getElementById("skillTreeDiv").clientWidth / 2;
-let lastPosy = document.getElementById("skillTreeDiv").clientHeight - 500;
+var spacing = 10;
+var lastPosX = document.getElementById("skillTreeDiv").clientWidth / 2;
+var lastPosy = document.getElementById("skillTreeDiv").clientHeight - 500;
+var boxWidth = 100;
+var boxHeight = 50;
 
-let longestBranchFirst;
+var maxMatrixDepth = 20;
+
+var longestBranchFirst;
+
+var levelDivs = [];
 
 fetch("../../input.json")
     .then(response => response.json())
@@ -44,37 +47,104 @@ fetch("../../input.json")
                 branchDiv.id = `branchDiv${divCounter}`;
                 divCounter++;
                 document.getElementById("skillTreeDiv").appendChild(branchDiv);
-                addElement(abilityMatrix, findNameInMatrix(abilityMatrix, longestBranchFirst[i][0]), level, branchDiv);
+                addElement(abilityMatrix, findNameInMatrix(abilityMatrix, longestBranchFirst[i][0]), level, branchDiv, true, false);
             }
         }
 
+        center()
+        drawLines();
+
     });
 
-function addElement(abilityMatrix, elementNr, level, branchDiv) {
-    //console.log(abilityMatrix[elementNr][0], elementNr, findAndAdd(abilityMatrix[elementNr][0], false), "pre")
+function drawLines() {
+    let localLevelDivs = levelDivs.reverse();
+    //TODO: Clear canvas
+    var dimension = [document.documentElement.clientWidth, document.documentElement.clientHeight];
+    var c = document.getElementById("canvas");
+    c.width = dimension[0];
+    c.height = dimension[1];
+
+    var ctx = c.getContext("2d");
+    //var ctx = canvas.getContext("2d");
+    //ctx.fillStyle = "#FF0000";
+
+    for (let p = 0; p < localLevelDivs.length; p++) {
+        let children = localLevelDivs[p].childNodes;
+        for (let i = 0; i < children.length; i++) {
+            let childrenRelated = getRelatedChildren(children[i].firstChild.innerText, p, localLevelDivs);
+            let rect = children[i].getBoundingClientRect(); rect.width
+            let x = Math.ceil(rect.left + rect.width / 2);
+            let y = rect.top + rect.height;
+            console.log(x, y);
+            ctx.moveTo(x, y);
+            ctx.lineTo(x, y + 10);
+            ctx.stroke();
+        }
+    }
+}
+
+function getRelatedChildren(name, level, localLevelDivs) {
+    let result = [];
+    for (let i = 0; i < localLevelDivs[level + 1].length; i++) {
+        if (localLevelDivs[level + 1][i].firstChild.innerText = name) {
+
+        }
+    }
+}
+
+function center() {
+    //console.log(levelDivs)
+    for (let p = 0; p < levelDivs.length; p++) {
+
+
+        let children = levelDivs[p].childNodes;
+        let right = false;
+        let offset = 0;
+        for (let i = 0; i < children.length; i++) {
+            let marginLeft = children[i].style.marginLeft == undefined ? 0 : children[i].style.marginLeft.slice(0, children[i].style.marginLeft.length - 2)
+            //console.log(children[i].style.marginLeft.slice(0, children[i].style.marginLeft.length - 2))
+            if (children[i].classList.contains("centered")) {
+                right = true;
+            } else if (right) {
+                offset += (parseInt(Math.ceil(children[i].getBoundingClientRect().width)) + parseInt(marginLeft)) / 2 + 1;
+            } else {
+                offset -= (parseInt(Math.ceil(children[i].getBoundingClientRect().width)) + parseInt(marginLeft)) / 2 + 1;
+            }
+        }
+        for (let i = 0; i < children.length; i++) {
+            children[i].style.left = offset + "px";
+        }
+    }
+}
+function addElement(abilityMatrix, elementNr, level, branchDiv, centered, left) {
     if (findAndAdd(abilityMatrix[elementNr][0], false) != true) {
         findAndAdd(abilityMatrix[elementNr][0], true);
-        createElement(abilityMatrix[elementNr][0], level, branchDiv);
-        /*
-                console.log("Start", abilityMatrix[elementNr]);
-                for (let i = 0; i < abilityMatrix.length; i++) {
-                    if (abilityMatrix[i][elementNr - 2]) {
-                        console.log(abilityMatrix[i], elementNr - 2, abilityMatrix[i][elementNr - 2]);
-                        if (level > 5) {
-                            console.log("STOOOOP")
-                        } else {
-                            addElement(abilityMatrix, i, level - 1, branchDiv);
-                        }
-                    }
-                }
-        */
-        for (let i = 1; i < abilityMatrix[elementNr].length; i++) {
-            if (abilityMatrix[elementNr][i]) {
-                if (level > 5) {
+        createElement(abilityMatrix[elementNr][0], level, branchDiv, centered, left);
+
+        let count = 0;
+        for (let i = 0; i < abilityMatrix.length; i++) {
+            if (abilityMatrix[i][elementNr + 1]) {
+                count++;
+            }
+        }
+        for (let i = 0; i < abilityMatrix.length; i++) {
+            if (abilityMatrix[i][elementNr + 1]) {
+                if (elementNr == 0) console.log(i, elementNr);
+                if (level > maxMatrixDepth) {
                     console.log("STOOOOP")
                 } else {
+                    let addLeft = i > count / 2 ? true : false;
+                    addElement(abilityMatrix, i, level - 1, branchDiv, false, addLeft);
+                }
+            }
+        }
 
-                    addElement(abilityMatrix, i - 1, level + 1, branchDiv);
+        for (let i = 1; i < abilityMatrix[elementNr].length; i++) {
+            if (abilityMatrix[elementNr][i]) {
+                if (level > maxMatrixDepth) {
+                    console.log("STOOOOP")
+                } else {
+                    addElement(abilityMatrix, i - 1, level + 1, branchDiv, true, false);
                 }
             }
         }
@@ -84,16 +154,30 @@ function addElement(abilityMatrix, elementNr, level, branchDiv) {
 
 }
 
-function createElement(name, level, branchDiv) {
+function createElement(name, level, branchDiv, centered, left) {
     let parentDiv = createLevelDiv(branchDiv, level);
-    let span = document.createElement("span");
-    //span.style.left = lastPosX + "px";
-    //span.style.top = lastPosy + "px";
-    //lastPosy -= offsetY - boxHeigt;
-    span.innerText = name;
+    let div = document.createElement("div");
+    div.innerHTML = "<p>" + name + "</p>";
+    div.style.margin = "5px";
+    div.style.display = "inline-block"
+    div.style.height = boxHeight + "px";
+    div.style.width = boxWidth + "px";
+    div.style.border = "5px red";
+    div.style.position = "relative";
+    div.style.backgroundColor = "grey";
+    div.style.borderRadius = "5px";
+    div.classList.add("nodeElement");
+
+
+    if (centered) div.classList.add("centered");
     findAndAdd(name, true);
-    parentDiv.appendChild(span);
+    if (left) {
+        parentDiv.insertBefore(div, parentDiv.firstChild);
+    } else {
+        parentDiv.appendChild(div);
+    }
     //document.getElementById("skillTreeDiv").appendChild(span);
+    return parentDiv;
 }
 
 function createLevelDiv(branchDiv, level) {
@@ -102,7 +186,9 @@ function createLevelDiv(branchDiv, level) {
         levelDiv = document.createElement("div");
         levelDiv.id = `${branchDiv.id}Level${level}`;
         levelDiv.class = "grid";
+        levelDiv.style.marginBottom = "20px";
         branchDiv.appendChild(levelDiv);
+        levelDivs.push(levelDiv);
     }
     return document.getElementById(`${branchDiv.id}Level${level}`);
 }
